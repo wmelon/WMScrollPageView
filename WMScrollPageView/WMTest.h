bd144f4029d3fb388ff580c6310462ad7eab1d08
陈仕家 <511863244@qq.com>
2017-09-01 17:06:17 +0800

    修改了bug代码

---
+++ WMScrollPageView/WMTest.h
@@ -0,0 +1,16 @@
+//
+//  WMTest.h
+//  WMDemo
+//
+//  Created by Sper on 2017/8/31.
+//  Copyright © 2017年 WM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMTest : NSObject
+- (void)sayHelloWorld;
+- (void)sayHi;
+
+@end
+
